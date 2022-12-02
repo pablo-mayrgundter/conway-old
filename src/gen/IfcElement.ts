fd80a83114f3443414fa78012af82df04a2cf93a
Conor Stokes <conor.stokes@burningcandle.io>
2022-12-03 03:21:38 +0800

    * Deleted old pre-codegen IfcWall, IfcElement and model_specification.

--- src/gen/IfcElement.ts
+++
@@ -1,7 +0,0 @@
-import Component from "../core/component";
-import { ModelIfcSpecification } from "./model_ifc";
-
-export interface IfcElement extends Component< ModelIfcSpecification >
-{
-    __type__ : 'IfcElement';
-}
--- src/gen/IfcWall.ts
+++
@@ -1,7 +0,0 @@
-import Component from "../core/component";
-import { ModelIfcSpecification } from "./model_ifc";
-
-export interface IfcWall extends Component< ModelIfcSpecification >
-{
-    __type__ : 'IfcWall';
-}
