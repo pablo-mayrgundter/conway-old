419ee716ec77cce73743649664d84a7b55d2eeae
nickcastel50 <nickcastel50@gmail.com>
2024-10-07 20:56:21 -0400

    Remove logging

--- src/step/step_model_base.ts
+++ src/step/step_model_base.ts
@@ -61,9 +61,6 @@ implements Iterable<BaseEntity>, Model {
 
     let where = 0
 
-    elementIndex.filter( (errorEntity) => errorEntity.typeID === void 0 )
-        .forEach( (errorEntity) => console.log( `Cannot find type: #${errorEntity.expressID}`) )
-
     const firstInlineElement = localElementIndex.length
 
     while (where < localElementIndex.length) {
